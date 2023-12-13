b04d10c468ba6dedd259d0742fcbcaa91879c20b
helderferreirarem <helder.ferreira@remardo.com>
2023-12-13 18:14:55 +0000

    Data Store Interface

---
+++ DSW.HDWallet.ConsoleApp/Domain/IDataStore.cs
@@ -0,0 +1,14 @@
+﻿using DSW.HDWallet.ConsoleApp.Domain.Models;
+
+namespace DSW.HDWallet.ConsoleApp.Domain
+{
+    public interface IDataStore
+    {
+        void SaveChanges();
+        List<Wallet> Wallets { get; }
+        List<CoinAddress> CoinAddresses { get; }
+        List<Rate> Rates { get; }
+        List<WalletCoin> WalletCoins { get; }
+        List<Setting> Settings { get; }
+    }
+}
