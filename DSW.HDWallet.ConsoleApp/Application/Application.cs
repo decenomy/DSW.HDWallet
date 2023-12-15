3d383f31361eae8a28640565b2a97593098e75d8
helderferreirarem <helder.ferreira@remardo.com>
2023-12-15 10:56:19 +0000

    Read storage at Console app Startup Check if wallet exists

--- DSW.HDWallet.ConsoleApp/Application/Application.cs
+++ DSW.HDWallet.ConsoleApp/Application/Application.cs
@@ -7,14 +7,49 @@ namespace DSW.HDWallet.ConsoleApp.Application
     {
         private readonly IWalletService walletService;
         private readonly IWalletManagerService walletManagerService;
+        private readonly IDataStore dataStore;
 
-        public Application(IWalletService walletService, IWalletManagerService walletManager)
+        public Application(IWalletService walletService, IWalletManagerService walletManager, IDataStore dataStore)
         {
             this.walletService = walletService;
             this.walletManagerService = walletManager;
+            this.dataStore = dataStore;
         }
 
         public void Run()
+        {
+            if (dataStore.Wallets.Any())
+            {
+                DisplayOptionsForExistingWallet();
+            }
+            else
+            {
+                DisplayOptionsForNewWallet();
+            }
+        }
+
+        private void DisplayOptionsForExistingWallet()
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("3: Delete Wallet");
+            Console.WriteLine("4: Add Coin");
+            var choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "3":
+                    DeleteWallet();
+                    break;
+                case "4":
+                    AddCoin();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+        }
+
+        private void DisplayOptionsForNewWallet()
         {
             Console.WriteLine("Choose an option:");
             Console.WriteLine("1: Create Wallet");
@@ -24,23 +59,46 @@ namespace DSW.HDWallet.ConsoleApp.Application
             switch (choice)
             {
                 case "1":
-                    Console.WriteLine("Enter a password (or leave blank):");
-                    var createPassword = Console.ReadLine();
-                    var wallet = walletManagerService.CreateWallet(createPassword);
-                    Console.WriteLine($"Wallet created. Mnemonic: {wallet}");
+                    CreateWallet();
                     break;
                 case "2":
-                    Console.WriteLine("Enter Mnemonic:");
-                    var mnemonic = Console.ReadLine();
-                    Console.WriteLine("Enter a password (or leave blank):");
-                    var recoverPassword = Console.ReadLine();
-                    var recoveredWallet = walletManagerService.RecoverWallet(mnemonic ?? "", recoverPassword);
-                    Console.WriteLine($"Wallet recovered: {recoveredWallet}");
+                    RecoverWallet();
                     break;
                 default:
                     Console.WriteLine("Invalid choice.");
                     break;
             }
         }
+
+        private void CreateWallet()
+        {
+            Console.WriteLine("Enter a password (or leave blank):");
+            var createPassword = Console.ReadLine();
+            var wallet = walletManagerService.CreateWallet(createPassword);
+            Console.WriteLine($"Wallet created. Mnemonic: {wallet}");
+        }
+
+        private void RecoverWallet()
+        {
+            Console.WriteLine("Enter Mnemonic:");
+            var mnemonic = Console.ReadLine();
+            Console.WriteLine("Enter a password (or leave blank):");
+            var recoverPassword = Console.ReadLine();
+            var recoveredWallet = walletManagerService.RecoverWallet(mnemonic ?? "", recoverPassword);
+            Console.WriteLine($"Wallet recovered: {recoveredWallet}");
+        }
+
+        private void DeleteWallet()
+        {
+            // Mock-up for deleting wallet
+            Console.WriteLine("Wallet deleted.");
+        }
+
+        private void AddCoin()
+        {
+            // Mock-up for adding a coin
+            Console.WriteLine("Coin added.");
+        }
     }
+
 }
--- DSW.HDWallet.ConsoleApp/Infrastructure/DataStore.cs
+++ DSW.HDWallet.ConsoleApp/Infrastructure/DataStore.cs
@@ -10,7 +10,7 @@ namespace DSW.HDWallet.ConsoleApp.Infrastructure
         public class DataStore : IDataStore
         {
             private readonly string _filePath;
-            private Dictionary<string, object> _data;
+            private Dictionary<string, JsonElement> _data;
 
             public DataStore()
             {
@@ -21,66 +21,58 @@ namespace DSW.HDWallet.ConsoleApp.Infrastructure
             public List<Wallet> Wallets
             {
                 get => GetCollection<Wallet>(nameof(Wallets));
-                set => _data[nameof(Wallets)] = value;
+                set => _data[nameof(Wallets)] = JsonSerializer.SerializeToElement(value);
             }
 
             public List<CoinAddress> CoinAddresses
             {
                 get => GetCollection<CoinAddress>(nameof(CoinAddresses));
-                set => _data[nameof(CoinAddresses)] = value;
+                set => _data[nameof(CoinAddresses)] = JsonSerializer.SerializeToElement(value);
             }
 
             public List<Rate> Rates
             {
                 get => GetCollection<Rate>(nameof(Rates));
-                set => _data[nameof(Rates)] = value;
-            }
-
-            public List<WalletCoin> WalletCoins
-            {
-                get => GetCollection<WalletCoin>(nameof(WalletCoins));
-                set => _data[nameof(WalletCoins)] = value;
+                set => _data[nameof(Rates)] = JsonSerializer.SerializeToElement(value);
             }
 
             public List<Setting> Settings
             {
                 get => GetCollection<Setting>(nameof(Settings));
-                set => _data[nameof(Settings)] = value;
+                set => _data[nameof(Settings)] = JsonSerializer.SerializeToElement(value);
+            }
+
+            public List<WalletCoin> WalletCoins
+            {
+                get => GetCollection<WalletCoin>(nameof(WalletCoins));
+                set => _data[nameof(WalletCoins)] = JsonSerializer.SerializeToElement(value);
             }
 
             public void SaveChanges()
             {
                 var options = new JsonSerializerOptions { WriteIndented = true };
-                var json = JsonSerializer.Serialize(_data, options);
+                var json = JsonSerializer.Serialize(_data.ToDictionary(pair => pair.Key, pair => JsonDocument.Parse(pair.Value.GetRawText())), options);
                 File.WriteAllText(_filePath, json);
             }
 
-            private Dictionary<string, object> LoadData()
+            private Dictionary<string, JsonElement> LoadData()
             {
-                if (!File.Exists(_filePath)) return new Dictionary<string, object>();
+                if (!File.Exists(_filePath)) return new Dictionary<string, JsonElement>();
 
                 var json = File.ReadAllText(_filePath);
-                return JsonSerializer.Deserialize<Dictionary<string, object>>(json) ?? new Dictionary<string, object>();
+                var data = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(json);
+
+                return data ?? new Dictionary<string, JsonElement>();
             }
 
             private List<T> GetCollection<T>(string key) where T : new()
             {
-                if (!_data.TryGetValue(key, out var collection))
+                if (_data.TryGetValue(key, out var element) && element.ValueKind == JsonValueKind.Array)
                 {
-                    collection = new List<T>();
-                    _data[key] = collection;
+                    return JsonSerializer.Deserialize<List<T>>(element.GetRawText()) ?? new List<T>();
                 }
 
-                if (collection is List<T> typedCollection)
-                {
-                    return typedCollection;
-                }
-                else
-                {
-                    var newCollection = new List<T>();
-                    _data[key] = newCollection;
-                    return newCollection;
-                }
+                return new List<T>();
             }
         }
     }
