845e47fee4bb85472821db4c0a0197bbcb4970b0
helderferreirarem <helder.ferreira@remardo.com>
2023-12-15 17:01:33 +0000

    Application Flow

--- DSW.HDWallet.ConsoleApp/Application/Application.cs
+++ DSW.HDWallet.ConsoleApp/Application/Application.cs
@@ -23,32 +23,50 @@ namespace DSW.HDWallet.ConsoleApp.Application
             {
                 if (dataStore.HasWallets())
                 {
-                    DisplayOptionsForExistingWallet();
+                    DisplayHomeScreenWithWallet();
                 }
                 else
                 {
-                    DisplayOptionsForNewWallet();
+                    DisplayHomeScreenWithoutWallet();
                 }
             }
         }
 
-        private void DisplayOptionsForExistingWallet()
+        private void DisplayHomeScreenWithWallet()
+        {
+            Console.WriteLine("Balance: $1000 (mocked)");
+            Console.WriteLine("My Coins: BTC, ETH (mocked)");
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1: Delete Wallet");
+            Console.WriteLine("2: Add Coin");
+            Console.WriteLine("3: Select Coin");
+            Console.WriteLine("4: Exit App");
+            HandleHomeScreenWithWalletChoices(Console.ReadLine() ?? "");
+        }
+
+        private void DisplayHomeScreenWithoutWallet()
         {
             Console.WriteLine("Choose an option:");
-            Console.WriteLine("3: Delete Wallet");
-            Console.WriteLine("4: Add Coin");
-            Console.WriteLine("5: Exit App");
-            var choice = Console.ReadLine();
+            Console.WriteLine("1: Create Wallet");
+            Console.WriteLine("2: Recover Wallet");
+            Console.WriteLine("3: Exit App");
+            HandleHomeScreenWithoutWalletChoices(Console.ReadLine() ?? "");
+        }
 
+        private void HandleHomeScreenWithWalletChoices(string choice)
+        {
             switch (choice)
             {
-                case "3":
+                case "1":
                     DeleteWallet();
                     break;
-                case "4":
-                    AddCoin();
+                case "2":
+                    DisplayAddCoinScreen();
                     break;
-                case "5":
+                case "3":
+                    DisplaySelectCoinScreen();
+                    break;
+                case "4":
                     exitApp = true;
                     break;
                 default:
@@ -57,14 +75,8 @@ namespace DSW.HDWallet.ConsoleApp.Application
             }
         }
 
-        private void DisplayOptionsForNewWallet()
+        private void HandleHomeScreenWithoutWalletChoices(string choice)
         {
-            Console.WriteLine("Choose an option:");
-            Console.WriteLine("1: Create Wallet");
-            Console.WriteLine("2: Recover Wallet");
-            Console.WriteLine("5: Exit App");
-            var choice = Console.ReadLine();
-
             switch (choice)
             {
                 case "1":
@@ -73,13 +85,44 @@ namespace DSW.HDWallet.ConsoleApp.Application
                 case "2":
                     RecoverWallet();
                     break;
-                case "5":
+                case "3":
                     exitApp = true;
                     break;
                 default:
                     Console.WriteLine("Invalid choice.");
                     break;
             }
+        }
+
+        private void DisplayAddCoinScreen()
+        {
+            Console.WriteLine("Available Coins: BTC, ETH, XRP (mocked)");
+            Console.WriteLine("Select Coin to Add:");
+            Console.WriteLine("1: BTC");
+            Console.WriteLine("2: ETH");
+            Console.WriteLine("3: XRP");
+            Console.WriteLine("4: Back");
+            var choice = Console.ReadLine();
+            if (choice == "4")
+            {
+                return; // Go back to home
+            }
+            AddCoin();
+        }
+
+        private void DisplaySelectCoinScreen()
+        {
+            Console.WriteLine("Balance: $500 (mocked)");
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1: Send");
+            Console.WriteLine("2: Receive");
+            Console.WriteLine("3: Back");
+            var choice = Console.ReadLine();
+            if (choice == "3")
+            {
+                return; // Go back to home
+            }
+            Console.WriteLine($"Option {choice} selected (mocked).");
         }
         private void CreateWallet()
         {
