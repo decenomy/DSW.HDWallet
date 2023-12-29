e282d3f7a787877d220dfeddd8c402fb299cf91c
helderferreirarem <helder.ferreira@remardo.com>
2023-12-29 16:53:07 +0000

    CurrencyVS object

---
+++ DSW.HDWallet/Domain/Models/CurrencyVS.cs
@@ -0,0 +1,15 @@
+﻿namespace DSW.HDWallet.Domain.Models
+{
+    public class CurrencyVS
+    {
+        public List<string> GetCurrencyTickers()
+        {
+            return new List<string>
+            {
+                "usd",
+                "eur",
+                "btc"
+            };
+        }
+    }
+}
