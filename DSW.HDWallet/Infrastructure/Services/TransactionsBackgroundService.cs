82744ab5f02bdf5950e425a725394e8cc071a30e
helderferreirarem <helder.ferreira@remardo.com>
2024-01-16 13:48:33 +0000

    Transaction Background Service V1

---
+++ DSW.HDWallet/Infrastructure/Services/TransactionsBackgroundService.cs
@@ -0,0 +1,55 @@
+﻿using DSW.HDWallet.Infrastructure.Api;
+using DSW.HDWallet.Infrastructure.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace DSW.HDWallet.Infrastructure.Services
+{
+    public class TransactionsBackgroundService : BaseBackgroundService<TransactionsBackgroundService>
+    {
+        private readonly IBlockbookHttpClient blockbookHttpClient;
+        private readonly IStorage storage;
+
+        public TransactionsBackgroundService(
+            IBlockbookHttpClient blockbookHttpClient,
+            IStorage storage,
+            ILogger<TransactionsBackgroundService> logger
+        ) : base(logger, "0 */1 * * * *") //Cron expression to make the service run every 5 minutes
+        {
+            this.blockbookHttpClient = blockbookHttpClient;
+            this.storage = storage;
+        }
+
+        protected override async Task OnExecute(CancellationToken cancellationToken)
+        {
+            logger.LogTrace("Rates Update Service executing.");
+
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    //Logic here
+                    var wallets = await storage.GetAllWallets();
+
+                    foreach (var wallet in wallets)
+                    {
+                            //GetXpub from wallet pubkey
+
+                            //loop through TXIDS
+                            //check if txID is already in database
+                            //Save if its not
+
+                    }
+
+                    var t = DateTime.Now;
+                    await Task.Delay(schedule.GetNextOccurrence(t) - t, cancellationToken);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+            }
+
+            logger.LogTrace("Rates Update Service executed.");
+        }
+    }
+}
