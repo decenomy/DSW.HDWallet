58b7c0870712429d192cab47c0f2ee52090fd368
helderferreirarem <helder.ferreira@remardo.com>
2024-01-23 16:11:46 +0000

    Delete unused Method

--- DSW.HDWallet/Infrastructure/Interfaces/ITransactionManager.cs
+++ DSW.HDWallet/Infrastructure/Interfaces/ITransactionManager.cs
@@ -4,7 +4,6 @@ namespace DSW.HDWallet.Infrastructure.Interfaces
 {
     public interface ITransactionManager
     {
-        Task<IEnumerable<object>> GetTransactions(string v);
         Task<OperationResult> SendCoins(string ticker, decimal numberOfCoins, string address, string? password);
     }
 }
