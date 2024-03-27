53b229290a5ffd3fcdcc4998e60271a2a339becb
helderferreirarem <helder.ferreira@remardo.com>
2024-03-27 12:51:51 +0000

    Fiat Calculator

---
+++ DSW.HDWallet/Domain/Utils/FiatCalculator.cs
@@ -0,0 +1,22 @@
+﻿
+using DSW.HDWallet.Domain.Models;
+
+namespace DSW.HDWallet.Domain.Utils
+{
+    public static class FiatCalculator
+    {
+
+        public static decimal GetFiatValue(long amount, Rate rate)
+        {
+            decimal decimalAmount = SatoshiConverter.FromSubSatoshi(amount);
+            decimal decimalRateValue = SatoshiConverter.FromSatoshi(rate.RateValue);
+
+            // Perform the conversion
+            decimal fiatValue = decimalAmount * decimalRateValue;
+
+            return fiatValue;
+        }
+
+    }
+}
+
