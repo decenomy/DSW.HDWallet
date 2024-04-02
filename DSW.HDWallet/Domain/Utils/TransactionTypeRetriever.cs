256afeb295f970d3148b6c4462b645d85f216e9c
helderferreirarem <helder.ferreira@remardo.com>
2024-04-02 17:42:45 +0100

    Transaction Type return

---
+++ DSW.HDWallet/Domain/Utils/TransactionTypeRetriever.cs
@@ -0,0 +1,22 @@
+﻿using DSW.HDWallet.Domain.Models;
+using NBitcoin;
+
+namespace DSW.HDWallet.Domain.Utils
+{
+    public static class TransactionTypeRetriever
+    {
+
+        public static string GetTransactionType(TransactionType transactionType)
+        {
+            if(transactionType == TransactionType.Incoming)
+            {
+                return "Received";
+            }
+            else
+            {
+                return "Sent";
+            }
+        }
+
+    }
+}
