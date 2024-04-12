1f2cd87bbe7873e2f8ce2ae9941274a20103b1d3
helderferreirarem <helder.ferreira@remardo.com>
2024-04-12 14:03:06 +0100

    Separate Balance service from Background Service

---
+++ DSW.HDWallet/Infrastructure/Services/BalanceService.cs
@@ -0,0 +1,53 @@
+﻿using DSW.HDWallet.Application;
+using DSW.HDWallet.Domain.Utils;
+using DSW.HDWallet.Infrastructure.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace DSW.HDWallet.Infrastructure.Services
+{
+    public class BalanceService : IBalanceService
+    {
+        private readonly IStorage storage;
+        private readonly IWalletService walletService;
+        private readonly ILogger<BalanceService> logger;
+
+        public BalanceService(IStorage storage, IWalletService walletService, ILogger<BalanceService> logger)
+        {
+            this.storage = storage;
+            this.walletService = walletService;
+            this.logger = logger;
+        }
+
+        public async Task UpdateAllBalancesAsync()
+        {
+            var walletCoins = await storage.GetAllWallets();
+            foreach (var coin in walletCoins)
+            {
+                if (coin.Ticker != null && coin.PublicKey != null)
+                {
+                    try
+                    {
+                        var balance = await walletService.GetXpub(coin.Ticker, coin.PublicKey, 1, 1);
+
+                        if (long.TryParse(balance.Balance, out long balanceValue))
+                        {
+                            decimal realBalance = SatoshiConverter.FromSatoshi(balanceValue);
+                            coin.Balance = SatoshiConverter.ToSubSatoshi(realBalance);
+
+                            if (long.TryParse(balance.UnconfirmedBalance, out long unconfirmedBalanceValue))
+                            {
+                                decimal realUnconfirmedBalance = SatoshiConverter.FromSatoshi(unconfirmedBalanceValue);
+                                coin.UnconfirmedBalance = SatoshiConverter.ToSubSatoshi(realUnconfirmedBalance);
+                            }
+                            await storage.SaveBalance(coin);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError($"Error updating balance for {coin.Ticker}: {ex.Message}");
+                    }
+                }
+            }
+        }
+    }
+}
--- DSW.HDWallet/Infrastructure/Services/BalancesBackgroundService.cs
+++ DSW.HDWallet/Infrastructure/Services/BalancesBackgroundService.cs
@@ -1,79 +1,40 @@
-﻿using DSW.HDWallet.Application;
-using DSW.HDWallet.Domain.Utils;
-using DSW.HDWallet.Infrastructure.Interfaces;
+﻿using DSW.HDWallet.Infrastructure.Interfaces;
 using Microsoft.Extensions.Logging;
 
 namespace DSW.HDWallet.Infrastructure.Services
 {
     public class BalancesBackgroundService : BaseBackgroundService<BalancesBackgroundService>
     {
-        private readonly IStorage storage;
-        IWalletService walletService;
+        private readonly IBalanceService balanceService;
+
         public BalancesBackgroundService(
             ILogger<BalancesBackgroundService> logger,
-            IStorage storage,
-            IWalletService walletService
+            IBalanceService balanceService
         ) : base(logger, "0 */5 * * * *") //Cron expression to make the service run every 5 minutes
         {
-            this.storage = storage;
-            this.walletService = walletService;
+            this.balanceService = balanceService;
         }
 
         protected override async Task OnExecute(CancellationToken cancellationToken)
         {
             logger.LogTrace("Balance Update Service executing.");
 
-            while (!cancellationToken.IsCancellationRequested)
+            try
             {
-                try
+                while (!cancellationToken.IsCancellationRequested)
                 {
-                    var walletCoins = await storage.GetAllWallets();
+                    await balanceService.UpdateAllBalancesAsync();
 
-                    foreach (var coin in walletCoins)
-                    {
-                        if (coin.Ticker != null && coin.PublicKey != null)
-                        {
-                            var balance = await walletService.GetXpub(coin.Ticker, coin.PublicKey, 1, 1);
-
-                            if (long.TryParse(balance.Balance, out long balanceValue))
-                            {
-                                decimal realBalance = SatoshiConverter.FromSatoshi(balanceValue);
-                                coin.Balance = SatoshiConverter.ToSubSatoshi(realBalance);
-
-                                if (long.TryParse(balance.UnconfirmedBalance, out long unconfirmedBalanceValue))
-                                {
-                                    decimal realUnconfirmedBalance = SatoshiConverter.FromSatoshi(unconfirmedBalanceValue);
-                                    coin.UnconfirmedBalance = SatoshiConverter.ToSubSatoshi(realUnconfirmedBalance);
-                                }
-                                else
-                                {
-                                    logger.LogError("Error parsing unconfirmed balance.");
-                                }
-
-                                await storage.SaveBalance(coin);
-                            }
-                            else
-                            {
-                                logger.LogError("Error parsing balance.");
-                            }
-                        }
-                        else
-                        {
-                            logger.LogError("Existing data error on updating balance.");
-                        }
-                    }
+                    var t = DateTime.Now;
+                    await Task.Delay(schedule.GetNextOccurrence(t) - t, cancellationToken);
                 }
-                catch (Exception ex)
-                {
-                    logger.LogError(ex.Message);
-                }
-
-                var t = DateTime.Now;
-                await Task.Delay(schedule.GetNextOccurrence(t) - t, cancellationToken);
             }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+            }
+
             logger.LogTrace("Balance Update Service executed.");
         }
-
-
     }
 }
